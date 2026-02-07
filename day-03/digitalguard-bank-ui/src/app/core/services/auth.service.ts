ba20726d5b1df22a2a998436ad306a59ddb240e7
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-07 09:47:36 +0100

    auth service

--- day-03/digitalguard-bank-ui/src/app/core/services/auth.service.ts
+++ day-03/digitalguard-bank-ui/src/app/core/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable, signal } from "@angular/core";
-import { RegisterRequest } from "../../shared/models/auth.model";
-import { Observable } from "rxjs";
+import { AuthResponse, LoginRequest, MfaRequest, RegisterRequest } from "../../shared/models/auth.model";
+import { Observable, tap } from "rxjs";
 
 @Injectable({providedIn:'root'})
 export class AuthService{
@@ -12,7 +12,32 @@ export class AuthService{
     currentUserToken = signal<string | null>(localStorage.getItem('jwt_token'));
     isMfaRequired = signal<boolean>(false);
 
-    register(data: RegisterRequest):Observable<AuthRespo>{
+    register(data: RegisterRequest):Observable<AuthResponse>{
+        return this.http.post<AuthResponse>(this.apiUrl+'/register',data);
+    }
 
+    login(credentials:LoginRequest):Observable<AuthResponse>{
+        return this.http.post<AuthResponse>(this.apiUrl+'/login', credentials).pipe(
+            tap(res=>{
+                if(res.mfaRequired) this.isMfaRequired.set(true);
+            })
+        );
+    }
+
+    verifyPin(credentials: MfaRequest):Observable<AuthResponse>{
+        return this.http.post<AuthResponse>(`${this.apiUrl}/verify-pin`, credentials).pipe(
+            tap(res => {
+                if(res.token){
+                    localStorage.setItem('jwt_token', res.token);
+                    this.currentUserToken.set(res.token);
+                    this.isMfaRequired.set(false);
+                }
+            })
+        );
+    }
+
+    logout():void { 
+        localStorage.removeItem('jwt_token');
+        this.currentUserToken.set(null);
     }
 }
