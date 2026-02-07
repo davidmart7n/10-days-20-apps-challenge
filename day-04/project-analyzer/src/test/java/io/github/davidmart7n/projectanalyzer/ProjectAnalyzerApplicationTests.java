77a8d23033f00574a3c8cd4aca91a83421973da2
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-07 15:05:34 +0100

    first commit day 4 apps

---
+++ day-04/project-analyzer/src/test/java/io/github/davidmart7n/projectanalyzer/ProjectAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.davidmart7n.projectanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
