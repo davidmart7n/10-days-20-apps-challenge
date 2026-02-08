60abd2bdca96fd8809d0e2e057805cb7ecd7b1d3
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-08 09:07:03 +0100

    started ui!

---
+++ day-04/stack-advisor-ui/src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import { Theme } from '@primeng/themes';
+import  Aura  from '@primeng/themes/aura'
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    providePrimeNG(
+      {theme: {preset : Aura}}
+    )
+  ]
+};
