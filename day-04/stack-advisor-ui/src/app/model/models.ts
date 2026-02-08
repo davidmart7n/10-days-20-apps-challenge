c77bb822e9fa0fe265dc7d4849e617beb354aed6
davidmart7n <davidmartinencuentra@gmail.com>
2026-02-08 09:56:18 +0100

    structure & service

---
+++ day-04/stack-advisor-ui/src/app/model/models.ts
@@ -0,0 +1,16 @@
+
+export interface Project {
+    projectName: string,
+    projectType: string,
+    teamSkills: string,
+    mainRequirements: string
+}
+
+export interface StackReport {
+    backend: string,
+    frontend: string,
+    database: string,
+    reasoning: string,
+    estimatedCost: string,
+    estimatedTime: string
+}
---
+++ day-04/stack-advisor-ui/src/app/service/ai.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Project, StackReport } from "../model/models";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AiService {
+
+  constructor ( private http: HttpClient) {}
+
+  private apiUrl:string="http://localhost:8085/api/ai"
+
+  getRecommendation(project:Project):Observable<StackReport>{
+    return this.http.get<StackReport>(this.apiUrl);
+  }
+
+}
